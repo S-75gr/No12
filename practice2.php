8dbed58de1fda5ee78da82ca1697e4a5b64aaf21
AKIHIRO <heeero75@gmail.com>
2020-02-16 01:42:37 +0000

    No12

---
+++ practice2.php
@@ -0,0 +1,38 @@
+<?php
+
+//課題1
+$name = "AKIHIRO";
+if ($name = "AKIHIRO") {
+    echo "私はAKIHIROです。";
+} else {
+    echo "AKIHIROではありません。";
+}
+$total =0;
+
+
+//課題2
+for($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+
+//課題3
+$fruits = array("Orange", "Apple", "Kiwi", "Grape", "Banana");
+foreach($fruits as $Fruits){
+    echo "" . $Fruits;
+    echo "\n";
+}
+
+//課題4
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+  }
+}
