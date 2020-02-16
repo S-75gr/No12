6752f5e0623f70ed4e81ce5925fe183f46f14b12
AKIHIRO <heeero75@gmail.com>
2020-02-16 08:20:17 +0000

    Syusei

--- practice.php
+++ practice.php
@@ -9,9 +9,9 @@ echo $c;
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
 echo $array_month[7];
 
-$hello = "Hello,";
+$hello = "Hello, ";
 $name = "Akihiro";
-$world = "s World!";
+$world = "'s World!";
 echo $hello . $name . $world;
 
 $tech_boost = 'tech';
--- sum.php
+++ sum.php
@@ -1,11 +1,42 @@
 <?php
-function sum($max){
+
+function sum($max) {
+    
     $result = 0;
-    for ($i = 1; $i <= $max; $i++){
+    
+    for ($i = 1; $i <= 100; $i++ ){
         $result += $i;
     }
     
     return $result;
 }
-echo sum();
+echo sum(100);
 ?>
+
+<?php
+$string = "ABCDEF";
+echo strlen($string);
+?>
+
+<?php
+$string = "T love Rudy";
+$new_string = str_replace("Rudy", "PHP", $string);
+
+echo $new_string;
+?>
+
+<?php
+$array = array(1,2,3,4,5,6,7,8,9,10);
+echo count($array);
+?>
+
+<?php
+$array = array(2,5,9,7,3,1,8,6,4);
+asort($array);
+print_r($array);
+
+arsort($array);
+print_r($array);
+?>
+
+
